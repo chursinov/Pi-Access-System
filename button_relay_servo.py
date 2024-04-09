4dde12afdd6d04f5f51ca402c31b33ac37f4e96d
chursinov <chursinov98@gmail.com>
2024-04-09 18:51:02 +0300

    Added open_lock function

--- button_relay_servo.py
+++ button_relay_servo.py
@@ -1,17 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-BUTTON = 4
 RELAY = 17
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON,GPIO.IN)
 GPIO.setup(RELAY,GPIO.OUT)
 GPIO.output(RELAY, GPIO.HIGH)
 
-while True:
-    if GPIO.input(BUTTON) == GPIO.LOW:
-        GPIO.output(RELAY, GPIO.LOW)
-        time.sleep(3)
-        GPIO.output(RELAY, GPIO.HIGH)
+def open_lock()
+    GPIO.output(RELAY, GPIO.LOW)
+    time.sleep(3)
+    GPIO.output(RELAY, GPIO.HIGH)
